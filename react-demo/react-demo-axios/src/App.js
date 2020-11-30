03198e50ce9c11db12496a67fae880d2d7c06ce6
bramwell simiyu <simiyub.engineer@gmail.com>
2020-11-30 15:38:23 +0000

    reorg

--- react-demo/react-demo-axios/src/App.js
+++ react-demo/react-demo-axios/src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <button onClick={axiosDemo}>AXIOS DEMO</button>
-    </div>
-  );
+class Welcome extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <h3>Hello</h3>
+        <button onClick={asyncCallDemo}>AXIOS DEMO</button>
+      </div>
+    );
+  }
+  
+
+
+
+
+let result = "";
+
+const response =()=>{
+  return result;
 }
+
 
 const requestListener =()=> {
   //console.log(this.);
 }
+
+function resolveAfter2Seconds(){
+  return new Promise( resolve =>{
+    setTimeout(() =>{
+      resolve('resolved');
+    },2000);
+  });
+}
+async function asyncCallDemo(){
+  console.log("calling");
+  result = await resolveAfter2Seconds();
+}
 const axiosDemo =()=>{
 
   var requestObject = new XMLHttpRequest();
-  requestObject.addEventListener("load",requestListener);
-  requestObject.open("GET", "https://topskills.tech/react/");
-  requestObject.send();
+  requestObject.addEventListener("load",asyncCallDemo);
+  //requestObject.open("GET", "https://topskills.tech/react/");
+  //requestObject.send();
 }
 
 export default App;
