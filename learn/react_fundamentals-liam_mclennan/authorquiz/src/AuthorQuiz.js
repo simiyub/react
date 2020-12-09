7514cb305cc932ba5b1537d5f1f4262bb803a9cc
bramwell simiyu <simiyub.engineer@gmail.com>
2020-12-09 18:20:07 +0000

    author quiz code along

--- learn/react_fundamentals-liam_mclennan/authorquiz/src/AuthorQuiz.js
+++ learn/react_fundamentals-liam_mclennan/authorquiz/src/AuthorQuiz.js
@@ -2,12 +2,62 @@ import logo from './logo.svg';
  import './App.css';
 import './bootstrap.min.css';
 
-function AuthorQuiz() {
+function Hero(){
   return (
-    <div >
-      Author Quiz
+  <div className="jumbotron col-10 offset-1">
+    <h1>Author Quiz</h1>
+    <p>Please select the book written by ther author shown< /p>
+
+  </div>);
+};
+
+function Book({title}){
+  return(
+    <div className="answer">
+      <h4>{title}</h4>
     </div>
-  );
+  
+  )
 }
+function Turn ({author, books}) {
+return (<div className="row turn" style={{ backgroundColor:"white" }}>
+
+  <div className="col-4 offset-1">
+<img src={author.imageUrl} className="authorimage" alt="Author"/>
+  </div>
+  <div className="col-6">  
+    {books.map((title) => <Book title={title} key={title}/>)}
+    </div>
+
+
+  </div>
+)
+
+};
+function Continue() {
+  return <div/>
+};
+
+function Footer(){
+return (<div id="footer" className="row">
+<div className="col-12 off-set-1">
+  <p className="text-muted credit">
+    All images from <a href="http://commons.wikimedia.org/wiki/Main"/>
+  </p>
+</div>
+</div >)
+};
+
+
+function AuthorQuiz({turnData}) {
+  return (
+    <div className="container-fluid" >
+      <Hero/>
+      <Turn {...turnData }/>
+      <Continue/>
+      <Footer/>
+    </div>
+  ); 
+}  
 
 export default AuthorQuiz;
